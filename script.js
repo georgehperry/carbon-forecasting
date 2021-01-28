a03826add971f51123fcac75690121fe193cd429
George Perry <george.h.perry@hotmail.co.uk>
2021-01-28 09:35:09 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,102 @@
+
+const currentDateTime = new Date().toISOString();
+let dataInnerObj = {};
+let times = [];
+let forecast = [];
+let index = [];
+
+const request = new XMLHttpRequest();
+request.open(`GET`, `https://api.carbonintensity.org.uk/intensity/${currentDateTime}/fw24h`);
+request.send();
+
+request.addEventListener('load', function() {
+  const apiData = JSON.parse(this.responseText).data;
+  console.log(apiData);
+  apiData.forEach(getForecastValue);
+  apiData.forEach(getForecastIndex);
+
+  createChart();
+});
+
+function getForecastValue(item) {
+  times.push(convertToTime(item['from']));
+  forecast.push(item['intensity']['forecast']);
+}
+
+function getForecastIndex(item) {
+  index.push(item['intensity']['index']);
+}
+
+function convertToTime(timestamp) {
+  const time = timestamp.slice(11, 16);
+  return time;
+}
+
+function createChart() {
+  const ctx = document.getElementById('chart');
+  const chart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: times,
+      datasets: [{
+        label: 'Time',
+        data: forecast,
+        backgroundColor: bgGradient(),
+        borderColor: [
+            'rgba(0, 0, 0, 1)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      scales: {
+        xAxes: [{
+            gridLines: {
+              drawOnChartArea: false
+            },
+            scaleLabel: {
+              display: true,
+              labelString: 'Time',
+              fontSize: 16
+            }
+        }],
+        yAxes: [{
+          gridLines: {
+            drawOnChartArea: false
+          },
+          scaleLabel: {
+            display: true,
+            labelString: 'Amount of carbon to produce electricity',
+            fontSize: 16
+          }
+        }]
+      },
+      legend: {
+        display: false
+      }
+    }
+  });
+}
+
+function bgGradient() {
+  const canvas = document.getElementById('chart');
+  const ctx = canvas.getContext('2d');
+  let gradient = ctx.createLinearGradient(0, 0, window.innerWidth, 0);
+  let xInterval = 0;
+  index.forEach((item) => {
+    if (item === 'high') {
+      gradient.addColorStop(xInterval, '#ff150080');
+    } else if (item === 'moderate') {
+      gradient.addColorStop(xInterval, '#ffd00080');
+    } else if (item === 'low') {
+      gradient.addColorStop(xInterval, '#0bcf04B8');
+    } else {
+      gradient.addColorStop(xInterval, '#fff');
+    }
+    xInterval += 0.02;
+  });
+
+  return gradient;
+}
